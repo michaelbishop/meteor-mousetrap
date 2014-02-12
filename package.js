217e206807a10199a28d36438d80d745709d1072
Michael Bishop <michael@michaelsplace.net>
2014-02-11 16:15:27 -0800

    added global bind

--- package.js
+++ package.js
@@ -5,5 +5,7 @@ Package.describe({
 Package.on_use(function (api){
 	api.add_files('mousetrap.min.js', 'client');
 	api.add_files('export-mousetrap.js', 'client');
+	api.add_files('plugins/global-bind/mousetrap-global-bind.js', 'client');
+
 	api.export && api.export('Mousetrap');
 });
